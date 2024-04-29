97d520956855f5f74fa3ec2fd0f537009407f25c
Hoang Vu Bui <hoang.bui@productmadness.com>
2024-04-29 10:34:08 +0100

    add Deius boss to be every 2 hours on even hours

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -3,7 +3,7 @@ namespace PristonToolsEU;
 
 public partial class App : Application
 {
-	private const int Height = 800;
+	private const int Height = 850;
 	private const int Width = 600;
 
 	public App()
